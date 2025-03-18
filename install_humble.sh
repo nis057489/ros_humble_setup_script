c823f81f772bc9585a06d9d0f7b5fd7fdfca0be9
nis057489 <nis057489@gmail.com>
2025-03-18 10:00:36 +1000

    Update install_humble.sh

--- install_humble.sh
+++ install_humble.sh
@@ -2,7 +2,7 @@
 
 locale  # check for UTF-8
 
-sudo apt update && sudo apt install locales
+sudo apt update && sudo apt install -y locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -15,7 +15,6 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 sudo apt update
-
 sudo apt upgrade
 
 sudo apt install -y ros-humble-desktop ros-dev-tools
